6a614b7ffddb4fa27d5969fa8b682fd03a68a5fe
anjaekk <anjaekk@gmail.com>
2021-12-27 22:43:14 +0900

    Add: Sequence-generator

--- Python/python_data_model/Sequence_001.py
+++ Python/python_data_model/Sequence_001.py
@@ -25,3 +25,24 @@ print(code_list3) # [43, 95, 41, 42, 94, 64]
 
 code_list4 = list(filter(lambda x : x > 40, map(ord, chars)))
 print(code_list4) # [43, 95, 41, 42, 94, 64]
+
+
+
+# Generator 생성 - 다음에 반환할 값만 가지고 있음(메모리 유지를 하지 않아 메모리 사용량이 적음)
+import array 
+
+tuple = [ord(s) for s in chars] 
+print(tuple) # [43, 95, 41, 40, 42, 38, 94, 37, 36, 35, 64, 33]
+
+tuple_g = (ord(s) for s in chars)
+print(tuple_g) # <generator object <genexpr> at 0x7fa9cf32f9e0>
+print(next(tuple_g)) # 43
+print(next(tuple_g))
+print(next(tuple_g))
+print(next(tuple_g))
+print(next(tuple_g))
+print(next(tuple_g))
+print(next(tuple_g))
+print(next(tuple_g))
+print(next(tuple_g))
+print(next(tuple_g))
