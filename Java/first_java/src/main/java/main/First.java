0b99f4f86ceef0d3720b2abb54bb421ff4aa7f1e
anjaekk <anjaekk@gmail.com>
2022-12-18 13:10:48 +0900

    Update: java study

---
+++ Java/first_java/src/main/java/main/First.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.Scanner;
+
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        Scanner scanner = new Scanner(System.in); // 입력
+        String str = scanner.nextLine(); // 한줄을 입력받음, 변수 str
+        System.out.println(str);
+    }
+
+}
